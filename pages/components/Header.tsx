01fe93a7ca736b536f5e072ee449d66ce6265339
Fatima Camero <fcamero12@gmail.com>
2022-01-25 21:28:52 -0800

    create Nav component

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './Header.module.css'
+import Nav from './Nav'
 
 const Header = () => {
   return (
     <div className={styles.header}>
+      <Nav />
       <p><span className={styles.headerBold}>Boon</span> <span className={styles.headerLight}>Photography</span></p>
     </div>
   )

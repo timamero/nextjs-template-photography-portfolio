a6eba909c6379917e6f0bfb8f59e8e662f7a9a80
Fatima Camero <fcamero12@gmail.com>
2022-02-04 22:25:03 -0800

    position nav elements

--- pages/components/Nav.tsx
+++ pages/components/Nav.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Nav.module.css'
 
 const Nav = () => {
   return (
-    <nav>
-      <svg width="50" version="1.1" viewBox="0 0 210 210">
+    <nav id="mainNav">
+      <svg className={styles.menu} width="20" version="1.1" viewBox="0 0 210 210">
         <g id="XMLID_2_">
           <path id="XMLID_4_" d="M75,0H15C6.716,0,0,6.716,0,15v60c0,8.284,6.716,15,15,15h60c8.284,0,15-6.716,15-15V15   C90,6.716,83.284,0,75,0z"/>
           <path id="XMLID_6_" d="m75 120h-60c-8.284 0-15 6.716-15 15v60c0 8.284 6.716 15 15 15h60c8.284 0 15-6.716 15-15v-60c0-8.284-6.716-15-15-15z"/>
@@ -11,7 +12,7 @@ const Nav = () => {
           <path id="XMLID_10_" d="m195 120h-60c-8.284 0-15 6.716-15 15v60c0 8.284 6.716 15 15 15h60c8.284 0 15-6.716 15-15v-60c0-8.284-6.716-15-15-15z"/>
         </g>
       </svg>
-      <ul>
+      <ul className={styles.menuItemsWrapper}>
         <li>Home</li>
         <li>Portfolio</li>
         <li>About</li>
